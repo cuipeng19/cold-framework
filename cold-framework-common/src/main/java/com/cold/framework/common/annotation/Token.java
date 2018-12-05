b577b7683d8aafd2975c05972347862b808925b4
cuipeng <peng.cui@orange-king.com>
2018-12-05 15:28:16 +0800

    test commit

---
+++ cold-framework-common/src/main/java/com/cold/framework/common/annotation/Token.java
@@ -0,0 +1,15 @@
+package com.cold.framework.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author cuipeng 2018/12/5 15:25
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Token {
+    String[] exclude() default {};
+}
